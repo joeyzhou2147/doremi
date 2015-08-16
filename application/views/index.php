9daf2b825f40eaabe648d9d233d6200b455e67d3
yuzhou <joeyzhou2147@gmail.com>
2015-08-16 09:37:02 +0800

    test 4

---
+++ application/views/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuzhou
+ * Date: 2015/8/16
+ * Time: 9:03
+ */
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -63,26 +63,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		border: 1px solid #D0D0D0;
 		box-shadow: 0 0 8px #D0D0D0;
 	}
+
+        .center{
+            align-content: center;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
 	</style>
 </head>
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1 class="center">Welcome to Do Re Mi!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+        <p class="center">Search Music Id by Title, singer, or rating.</p>
+        <code><form class="center">Title: <input><button>Search</button></form></code>
+        <code><form class="center">Singer: <input><button>Search</button></form></code>
+        <code>Rating List:<form class="center">
+                <ul>
+                    <li>爱你一万年</li>
+                    <li>爱你一千年</li>
+                    <li>爱你一百年</li>
+                </ul></form></code>
+		<p class="center">If you are exploring DoReMi for the very fir    st time, you should start by reading the &nbsp;<a href="user_guide/">User Guide</a>.</p>
+</div>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'DoReMi Version <strong>' . '0.0.1' . '</strong>' : '' ?></p>
 </div>
 
 </body>
